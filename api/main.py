f4dbdc6d306d360787329ab200d136bb0779ed1b
PolinaScrbbs <polinascaraboobs@gmail.com>
2025-01-11 21:17:35 +0300

    Media

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
-
+from config import config as conf
 from .auth.router import router as authRouter
 from .content.router import router as contentRouter
 from .commissariat.router import router as commissariatRouter
@@ -11,6 +12,7 @@ app = FastAPI(
     version="2.2.8",
 )
 
+app.mount("/media", StaticFiles(directory=conf.media_root), name="media")
 
 app.include_router(authRouter, tags=["Auth"])
 app.include_router(contentRouter, tags=["Content"])

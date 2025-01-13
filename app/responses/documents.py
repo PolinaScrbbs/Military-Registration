f26d709cb670d7937e83179e9dea7fdccc5a5c29
PolinaScrbbs <polinascaraboobs@gmail.com>
2025-01-13 20:14:57 +0300

    Other Documents

--- app/responses/documents.py
+++ app/responses/documents.py
@@ -10,3 +10,10 @@ async def get_documents(category: str) -> Tuple[int, Optional[List[dict]]]:
             return response.status, (
                 await response.json() if response.status == 200 else None
             )
+
+async def get_archived_documents() -> Tuple[int, Optional[List[dict]]]:
+    async with aiohttp.ClientSession(API_URL) as session:
+        async with session.get(f"/contents/?archived={True}") as response:
+            return response.status, (
+                await response.json() if response.status == 200 else None
+            )
--- app/routers/documents.py
+++ app/routers/documents.py
@@ -1,6 +1,6 @@
 from quart import Blueprint, render_template, request, jsonify
 
-from ..responses.documents import get_documents
+from ..responses import get_documents, get_archived_documents
 
 documents_router = Blueprint("documents_router", __name__)
 
@@ -20,3 +20,13 @@ async def create_document_form(category: str):
     token = request.cookies.get("access_token")
     if not token:
         return jsonify({"message": "Unauthorized"}), 403
+
+
+@documents_router.route("/documents/archive")
+async def archive():
+    _, documents = await get_archived_documents()
+    context = {
+        "title": "Archive",
+        "documents": documents
+    }
+    return await render_template(f"documents.html", **context)
